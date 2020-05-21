8e70822224717071e4a6d66d59e41f6d7a7c3a3d
Tyler <tnewman820@gmail.com>
2020-05-21 16:41:06 -0400

    testing and api changes

---
+++ public_html/LAMPAPI/RegUser.php
@@ -0,0 +1,42 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$userId = $inData["username"];
+	$userId = $inData["password"];
+	$userId = $inData["phonenumber"];
+
+	$conn = new mysqli("localhost", "tnewman820", "Password00115!", "tnewman8_COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "insert into Database (Users) VALUES (" . $userId . ",'" . $username . "','" . $password . "', '" . $phonenumber . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>

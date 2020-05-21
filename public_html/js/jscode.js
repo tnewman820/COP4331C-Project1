2fb5ed703fee635399e5fa284891b0bfee084456
jesNav230 <64986634+jesNav230@users.noreply.github.com>
2020-05-21 15:51:10 -0400

    create jscode.js

---
+++ public_html/js/jscode.js
@@ -0,0 +1,102 @@
+var urlBase = 'http://branchout.space';
+var extension = 'php';
+
+var userId = 0;
+var fistname = "";
+var lastName = "";
+
+function doLogin()
+{
+	userId = 0;
+	fistname = "";
+	lastName = "";
+	
+	var login = document.getElementById("loginName").value;
+	var password = document.getElementById("loginPassword").value;
+//	var hash = md5( password );
+	
+	document.getElementById("loginResult").innerHTML = "";
+
+//	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+	var url = urlBase + '/Login.' + extension;
+
+	var request = new XMLHttpRequest();
+	request.open("POST", url, false);
+	requst.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		request.send(jsonPayload);
+		
+		var jsonObject = JSON.parse( request.responseText );
+		
+		userId = jsonObject.id;
+		
+		if( userId < 1 )
+		{
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+		}
+		
+		fistname = jsonObject.fistname;
+
+		saveCookie();
+	
+		window.location.href = "contacts.html";
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
+
+function saveCookie()
+{
+	var minutes = 20;
+	var date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "fistname=" + fistname + ", lastname=" + lastname + ", userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	var data = document.cookie;
+	var splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		var thisOne = splits[i].trim();
+		var tokens = thisOne.split("=");
+		if( tokens[0] == "fistname" )
+		{
+			fistname = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
